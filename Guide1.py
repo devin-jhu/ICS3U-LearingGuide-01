7554a75908625a74d92f09d7f6c12076b5d2d36b
Ubuntu <ubuntu@ip-172-31-85-29.ec2.internal>
2022-03-31 18:34:54 +0000

    code

---
+++ Guide1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by Devin Jhu
+# Created on February 2022
+# The space aliens game
+
+import stage
+
+
+def game_scene():
+    #this is the main game scene
+    print("\n\n\n")
+    print("Hello, World!")
+    
+    while True:
+        pass #repeat forever
+    
+
+
+if __name__ == "__main__":
+    game_scene()
