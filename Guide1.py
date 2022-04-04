41f7ae9c845ec3b416cf6197f59aac723cbe391e
Ubuntu <ubuntu@ip-172-31-85-29.ec2.internal>
2022-04-04 22:26:35 +0000

    added code again

--- Guide1.py
+++ Guide1.py
@@ -5,17 +5,27 @@
 # The space aliens game
 
 import stage
+import ugame
 
 
 def game_scene():
-    #this is the main game scene
-    print("\n\n\n")
-    print("Hello, World!")
-    
-    while True:
-        pass #repeat forever
-    
+# image banks for CircuitPython
+image bank background = stage.Bank. from bmp16("space aliens_background.bmp")
 
+# set the background to image 0 in the image bank
 
+# and the size (10x8 tiles of size 16x16)
+background = stage.Grid(image_bank_background, 10, 8)
+
+# create a stage for the background to show up on
+
+#and set the frame rate to 60fps
+game = stage.Stage(ugame.display, 60)
+# set the Layers of all sprites,
+items show up in order
+game.layers = [background]
+# render all sprites
+# most Likely you will only render the background once per game scene
+game.render_block()
 if __name__ == "__main__":
     game_scene()
