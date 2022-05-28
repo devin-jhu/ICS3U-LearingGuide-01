ce6fd68fc8fb3bc70564da1da49dcca6a67b3122
Devin Jhu <devin.jhu@mths.ca>
2022-05-28 16:34:34 +0000

    code

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by Devin Jhu
-# Created on April 2022
+# Created on May 2022
 # The space aliens game
 
 # PyBadge screen size is 160x128 and sprites are 16x16
@@ -11,15 +11,23 @@ SCREEN_GRID_X = 10
 SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 TOTAL_NUMBER_ALIENS = 5
+TOTAL_NUMBER_OF_LASERS = 5
+SHIP_SPEED = 1
+ALIEN_SPEED = 1
+LASER_SPEED = 2
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
 
-# Using for button state 
+# Using for button state
 button_state = {
     "button_up": "up",
     "button_just_pressed": "just pressed",
     "button_still_pressed": "still pressed",
-    "button_released": "released"
+    "button_released": "released",
 }
 
 # new pallet for filled text
